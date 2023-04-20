56d0c0769364e2c8f58159fb1c9e7da77ffd352d
Vinicius <vinifreitas.d.a@gmail.com>
2023-04-20 14:00:21 -0300

    chore: adapting robot controls to new API

--- src/main.rs
+++ src/main.rs
@@ -115,6 +115,14 @@ fn find_theta(y: &[f64; 5], l: f64, d: f64) -> f64 {
     ((0.5 + t) / d).atan()
 }
 
+const ROBOT_WHEEL_RADIUS: f64 = 0.04;
+const ROBOT_SIDE_LENGTH: f64 = 0.1;
+
+const SENSOR_ARRAY_LENGTH: f64 = ROBOT_SIDE_LENGTH * 1.1;
+const SENSOR_ARRAY_SEPARATION: f64 = SENSOR_ARRAY_LENGTH / 5.0;
+const MAX_SENSOR_DISTANCE: f64 = 4.0 * SENSOR_ARRAY_SEPARATION / 5.0;
+const SENSOR_DISTANCE_TO_ROBOT_CENTER: f64 = ROBOT_SIDE_LENGTH * 3.0 / 5.0;
+
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut show_egui_demo_windows = false;
@@ -132,7 +140,7 @@ async fn main() {
     let mut camera_center: Vec2 = Vec2::ZERO;
 
     // sample once per frame
-    let mut mouse_sdf_history = [0.0f32; 400];
+    let mut robot_sdf_history = [0.0f32; 400];
     let mut i = 0;
 
     let mut wl_history = [0.0f32; 400];
@@ -145,77 +153,23 @@ async fn main() {
     let mut prev_error = 0.0;
     let main_path_sdf = predefined_closed_path_sdf();
 
-    let robot_dynamics = |t: f64, x: &Vector<8>| {
-        let (x, y, theta, wl, dwl, wr, dwr, int_error) = (x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]);
-        const ROBOT_WHEEL_RADIUS: f64 = 0.04;
-        const ROBOT_SIDE_LENGTH: f64 = 0.1;
+    let robot_dynamics = |t: f64, x: &Vector<7>, u: &Vector<2>| {
+        let (x, y, theta, wl, dwl, wr, dwr) = (x[0], x[1], x[2], x[3], x[4], x[5], x[6]);
 
-        const SENSOR_ARRAY_LENGTH: f64 = ROBOT_SIDE_LENGTH * 1.1;
-        const SENSOR_ARRAY_SEPARATION: f64 = SENSOR_ARRAY_LENGTH / 5.0;
-        const MAX_SENSOR_DISTANCE: f64 = 4.0 * SENSOR_ARRAY_SEPARATION / 5.0;
-        const SENSOR_DISTANCE_TO_ROBOT_CENTER: f64 = ROBOT_SIDE_LENGTH * 3.0 / 5.0;
-
-        // we initially consider the robot pointing rightward, so the sensor array is vertical (x constant)
-        let mut sensor_positions = [
-            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, 2.0 * SENSOR_ARRAY_SEPARATION),
-            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, SENSOR_ARRAY_SEPARATION),
-            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, 0.0),
-            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, -SENSOR_ARRAY_SEPARATION),
-            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, -2.0 * SENSOR_ARRAY_SEPARATION),
-        ];
-
-        // now we rotate the sensor array by theta counter-clockwise
-        // and translate it by (x, y)
-        for p in sensor_positions.iter_mut() {
-            let rotation = Rotation2::new(theta);
-            let rotated = rotation * (*p);
-            *p = Vector2::<f64>::new(x, y) + rotated;
-        }
-
-        let mut sensor_distances = [0.0f64; 5];
-        for i in 0..5 {
-            if let Some(d) = main_path_sdf.sdf(sensor_positions[i].x, sensor_positions[i].y) {
-                sensor_distances[i] = d.abs();
-            } else {
-                sensor_distances[i] = 1e10;
-            }
-        }
-
-        const KP: f64 = 0.8;
-        const KI: f64 = 0.2;
-        const KD: f64 = 1.5;
-        
-        // estimate the robot's angle relative to the track 
-        // (i.e. the error in theta) by using the sensor array data
-        let error_estimate = find_theta(&sensor_distances, MAX_SENSOR_DISTANCE, ROBOT_SIDE_LENGTH / 2.0);
-        let deriv_error = if t > prev_t {
-            (error_estimate - prev_error) / (t - prev_t)
-        } else {
-            0.0
-        };
-         
-        prev_error = error_estimate;
-        prev_t = t;
-        let desired_dtheta = KP * error_estimate + KI * int_error + KD * deriv_error;
-        let k = ROBOT_SIDE_LENGTH * (B*R + K*K) / (K * ROBOT_WHEEL_RADIUS);
-        let u = 5.0;
-        let v = k * desired_dtheta;
-        let ul = (u - v) / 2.0;
-        let ur = (u + v) / 2.0;
-
-        if ur.is_nan() || ul.is_nan() {
-            panic!("nan");
-        }
-
-        const J: f64 = 0.1;
-        const B: f64 = 0.1;
-        const R: f64 = 0.03;
-        const L: f64 = 0.1;
-        const K: f64 = 0.1;
-
-        let c0: f64 = 1.0;
-        let c1: f64 = 1.414;
+        let w0: f64 = 10.0;
+        let xi: f64 = 0.71;
+        let c0: f64 = 1.0 / (w0*w0);
+        let c1: f64 = 2.0 * xi / w0;
         let c2: f64 = 1.0;
+         
+        let ul = u[0];
+        let ur = u[1];
+
+        // const J: f64 = 0.1;
+        // const B: f64 = 0.1;
+        // const R: f64 = 0.03;
+        // const L: f64 = 0.1;
+        // const K: f64 = 0.1;
 
         let speed = ROBOT_WHEEL_RADIUS * (wl + wr) / 2.0;
         let d_theta = ROBOT_WHEEL_RADIUS * (wr - wl) / ROBOT_SIDE_LENGTH;
@@ -227,14 +181,16 @@ async fn main() {
         let d_dwr = (ur - c1 * dwr - c2 * wr) / c0;
 
         //(x, y, theta, wl, dwl, wr, dwr, int_error)
-        Vector::<8>::from_column_slice(&[d_x, d_y, d_theta, d_wl, d_dwl, d_wr, d_dwr, error_estimate])
+        Vector::<7>::from_column_slice(&[d_x, d_y, d_theta, d_wl, d_dwl, d_wr, d_dwr])
     };
-    let initial_condition = Vector::<8>::from_column_slice(&[0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0]);
+    let initial_condition = Vector::<7>::from_column_slice(&[0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0]);
     let mut robot_integrator = Rk4::new(robot_dynamics, 0.0, initial_condition);
-
+    let mut int_error = 0.0;
     
 
     let main_path = predefined_closed_path();
+
+    let mut sim_result = initial_condition;
     
 
     loop {
@@ -271,7 +227,63 @@ async fn main() {
         let mouse_world_pos = camera.screen_to_world(macroquad::input::mouse_position().into());
 
         set_camera(&camera);
+        // control system
+        // we initially consider the robot pointing rightward, so the sensor array is vertical (x constant)
+        let mut sensor_positions = [
+            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, 2.0 * SENSOR_ARRAY_SEPARATION),
+            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, SENSOR_ARRAY_SEPARATION),
+            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, 0.0),
+            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, -SENSOR_ARRAY_SEPARATION),
+            Vector2::<f64>::new(SENSOR_DISTANCE_TO_ROBOT_CENTER, -2.0 * SENSOR_ARRAY_SEPARATION),
+        ];
 
+        // now we rotate the sensor array by theta counter-clockwise
+        // and translate it by (x, y)
+        for p in sensor_positions.iter_mut() {
+            let rotation = Rotation2::new(sim_result[2]);
+            let rotated = rotation * (*p);
+            *p = Vector2::<f64>::new(sim_result[0], sim_result[1]) + rotated;
+        }
+
+        let mut sensor_distances = [0.0f64; 5];
+        for i in 0..5 {
+            if let Some(d) = main_path_sdf.sdf(sensor_positions[i].x, sensor_positions[i].y) {
+                sensor_distances[i] = d.abs();
+            } else {
+                sensor_distances[i] = 1e10;
+            }
+        }
+        // estimate the robot's angle relative to the track 
+        // (i.e. the error in theta) by using the sensor array data
+        let error_estimate = find_theta(&sensor_distances, MAX_SENSOR_DISTANCE, ROBOT_SIDE_LENGTH / 2.0);
+        let deriv_error = (error_estimate - prev_error) * 60.0;
+        int_error += error_estimate * 1.0/60.0;
+
+
+        let w0: f64 = 10.0;
+        let xi: f64 = 0.71;
+        let c0: f64 = 1.0 / (w0*w0);
+        let c1: f64 = 2.0 * xi / w0;
+        let c2: f64 = 1.0;
+        const KP: f64 = 0.0003;
+        const KI: f64 = 0.0006;
+        const KD: f64 = 0.009;
+        let desired_dtheta = KP * error_estimate + KI * int_error + KD * deriv_error;
+        let k = ROBOT_SIDE_LENGTH * c2 / ROBOT_WHEEL_RADIUS;
+        let u_avg = 15.0;
+        let v = k * desired_dtheta;
+
+        let ul = (u_avg - v) / 2.0;
+        let ur = (u_avg + v) / 2.0;
+        // end control
+
+        robot_integrator.step(1.0/60.0, &Vector::<2>::from_column_slice(&[ul, ur]));
+        sim_result = robot_integrator.get_state();
+        wl_history[wl_i] = sim_result[3] as f32;
+        wl_i = (wl_i + 1) % wl_history.len();
+
+        wr_history[wr_i] = sim_result[5] as f32;
+        wr_i = (wr_i + 1) % wl_history.len();
         // draw egui
         zoom *= (mouse_wheel().1 * 0.1).exp();
 
@@ -305,15 +317,15 @@ async fn main() {
                 ui.label(format!("Mouse position: ({:.3}, {:.3})", mouse_x, mouse_y));
 
                 // show distance to path
-                let distance = main_path_sdf.sdf(mouse_x as f64, mouse_y as f64);
+                let distance = main_path_sdf.sdf(sim_result[0], sim_result[1]);
                 if let Some(d) = distance {
-                    mouse_sdf_history[i] = d as f32;
+                    robot_sdf_history[i] = d as f32;
                     ui.label(format!("Distance to path: {:.3}", d));
                 } else {
-                    mouse_sdf_history[i] = 0.0;
+                    robot_sdf_history[i] = 0.0;
                     ui.label("Distance to path: N/A");
                 }
-                i = (i + 1) % mouse_sdf_history.len();
+                i = (i + 1) % robot_sdf_history.len();
                 let (mouse_wheel_x, mouse_wheel_y) = mouse_wheel();
                 ui.label(format!("Mouse wheel: ({:.3}, {:.3})", mouse_wheel_x, mouse_wheel_y));
 
@@ -323,18 +335,18 @@ async fn main() {
                 }
             });
 
-            egui::Window::new("Plot").show(egui_ctx, |ui| {
-                let plot = egui::plot::Plot::new("debug_view_coordinates")
+            egui::Window::new("Robot distance to track").show(egui_ctx, |ui| {
+                let plot = egui::plot::Plot::new("debug_view_robot_distance")
                             .view_aspect(1.0)
                             .allow_zoom(false)
                             .allow_drag(false)
                             .allow_scroll(false)
                             .show_background(false)
-                            .include_y(2.0)
-                            .include_y(-2.0);
+                            .include_y(1.0)
+                            .include_y(-1.0);
                 plot.show(ui, |plot_ui| {
                     plot_ui.line(
-                        Line::new(PlotPoints::from_ys_f32(&mouse_sdf_history))
+                        Line::new(PlotPoints::from_ys_f32(&robot_sdf_history))
                             .color(egui::Color32::from_rgb(0, 0, 255))
                             .name("Distance to path")
                     )
@@ -342,7 +354,7 @@ async fn main() {
             });
 
             egui::Window::new("Omega_l plot").show(egui_ctx, |ui| {
-                let plot = egui::plot::Plot::new("debug_view_coordinates")
+                let plot = egui::plot::Plot::new("debug_view_omegas")
                             .view_aspect(1.0)
                             .allow_zoom(false)
                             .allow_drag(false)
@@ -375,13 +387,7 @@ async fn main() {
         }
         
         draw_path(&main_path, BLACK);
-        robot_integrator.step(1.0/60.0);
-        let sim_result = robot_integrator.get_state();
-        wl_history[wl_i] = sim_result[3] as f32;
-        wl_i = (wl_i + 1) % wl_history.len();
 
-        wr_history[wr_i] = sim_result[5] as f32;
-        wr_i = (wr_i + 1) % wl_history.len();
         draw_robot(sim_result[0] as f32, sim_result[1] as f32, sim_result[2] as f32 * 180.0 / PI, RED);
         egui_macroquad::draw();
 
