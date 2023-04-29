240da1f502adda0bc3d415b8a827d6c8c3392a68
Vinicius <vinifreitas.d.a@gmail.com>
2023-04-29 06:10:00 -0300

    feat: added simple canvas w/ zoom and camera

--- path_editor/src/app.rs
+++ path_editor/src/app.rs
@@ -1,5 +1,18 @@
+use crate::canvas::Canvas;
+
 #[derive(Default)]
-pub struct PathEditorApp {}
+pub struct PathEditorApp {
+    tool: Tool,
+    canvas: Canvas,
+}
+
+#[derive(Default, PartialEq)]
+enum Tool {
+    #[default]
+    Free,
+    ArcPath,
+    LinePath,
+}
 
 impl PathEditorApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -9,8 +22,20 @@ impl PathEditorApp {
 
 impl eframe::App for PathEditorApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
+        egui::CentralPanel::default()
+            .frame(egui::Frame::dark_canvas(&ctx.style()))
+            .show(ctx, |ui| {
+                self.canvas.ui(ui);
+            });
+        egui::Window::new("Tools").show(ctx, |ui| {
+            // Tool selector: either ArcPath or LinePath creators
+            ui.selectable_value(&mut self.tool, Tool::Free, "Free");
+            ui.selectable_value(&mut self.tool, Tool::ArcPath, "Arc Path");
+            ui.selectable_value(&mut self.tool, Tool::LinePath, "Line Path");
         });
+        // if the user presses ESC, the tool will switch to Free
+        if ctx.input(|i| i.key_pressed(egui::Key::Escape)) {
+            self.tool = Tool::Free;
+        }
     }
 }
---
+++ path_editor/src/canvas.rs
@@ -0,0 +1,125 @@
+// based on https://github.com/emilk/egui/blob/master/crates/egui_demo_app/src/wrap_app.rs lines 43-52
+use egui::{containers::*, widgets::*, *};
+use linefollower_core::{geometry::closed_path::SubPath, utils::math::sigmoid};
+
+pub struct Canvas {
+    pub subpaths: Vec<SubPath<f64>>,
+    zoom: f32,
+    focus_center: Pos2,
+}
+
+impl Default for Canvas {
+    fn default() -> Self {
+        Self {
+            subpaths: Vec::new(),
+            zoom: 1.0,
+            focus_center: Pos2::ZERO,
+        }
+    }
+}
+
+impl Canvas {
+    pub fn ui(&mut self, ui: &mut Ui) {
+        const MIN_ZOOM: f32 = 0.1;
+        const MAX_ZOOM: f32 = 10.0;
+        // get mouse scroll to adjust zoom
+        let scroll = ui.input(|i| i.scroll_delta);
+        // calculate zoom from mouse scroll
+        let mw = sigmoid(scroll.y) - 0.5;
+        let new_zoom = self.zoom * (mw * 0.1).exp();
+        if new_zoom <= MIN_ZOOM {
+            self.zoom = MIN_ZOOM;
+        } else if new_zoom >= MAX_ZOOM {
+            self.zoom = MAX_ZOOM;
+        } else {
+            self.zoom = new_zoom;
+        }
+        // use WASD to move the camera center of focus
+        let mut move_center = |mut dir: Vec2| {
+            dir.x /= self.zoom;
+            dir.y /= self.zoom;
+            self.focus_center += dir;
+        };
+        const SPEED: f32 = 0.01;
+        let mut v = Vec2::ZERO;
+        // TODO: check how to implement this withou "hiccups"
+        // I think the hiccups are caused by the fact that the UI is retained
+        // so it takes a while for new events to happen, which in turn
+        // makes you wait about 0.5 seconds before the repeated "Hold" event kicks in
+        if ui.input(|i| i.key_down(Key::W)) {
+            ui.label("W is down!");
+            v += vec2(0.0, 1.0);
+        }
+        if ui.input(|i| i.key_down(Key::A)) {
+            ui.label("A is down!");
+            v += vec2(-1.0, 0.0);
+        }
+        if ui.input(|i| i.key_down(Key::S)) {
+            ui.label("S is down!");
+            v += vec2(0.0, -1.0);
+        }
+        if ui.input(|i| i.key_down(Key::D)) {
+            ui.label("D is down!");
+            v += vec2(1.0, 0.0);
+        }
+        // normalize diagonal movement
+        if v != Vec2::ZERO {
+            v = v.normalized();
+            v.x *= SPEED;
+            v.y *= SPEED;
+        }
+        move_center(v);
+
+        //paint
+        let painter = Painter::new(
+            ui.ctx().clone(),
+            ui.layer_id(),
+            ui.available_rect_before_wrap(),
+        );
+        self.paint(&painter);
+        // Make sure we allocate what we used (everything)
+        ui.expand_to_include_rect(painter.clip_rect());
+        // get mouse position
+        let pos = ui.input(|i| i.pointer.hover_pos());
+        // show mouse position
+        if let Some(pos) = pos {
+            ui.label(format!("Mouse Position (screen coordinates) = ({:.1}, {:.1})", pos.x, pos.y));
+            // convert mouse position to canvas/world coordinates
+            let pos = self.screen_to_world(pos, &painter);
+            ui.label(format!("Mouse Position (canvas coordinates) = ({:.1}, {:.1})", pos.x, pos.y));
+        }
+    }
+    fn screen_to_world(&self, pos: Pos2, painter: &Painter) -> Pos2 {
+        let rect = painter.clip_rect();
+        let mut sqr_prop = rect.square_proportions() / self.zoom;
+        sqr_prop.y *= -1.0;
+        let to_world = emath::RectTransform::from_to(
+            rect,
+            Rect::from_center_size(Pos2::ZERO, sqr_prop),
+        );
+        to_world * pos
+    }
+    fn paint(&mut self, painter: &Painter) {
+        let mut shapes: Vec<Shape> = Vec::new();
+        let rect = painter.clip_rect();
+        let mut sqr_prop = rect.square_proportions() / self.zoom;
+        sqr_prop.y *= -1.0;
+        let to_screen = emath::RectTransform::from_to(
+            Rect::from_center_size(Pos2::ZERO, sqr_prop),
+            rect,
+        );
+        let mut paint_line = |points: [Pos2; 2], color: Color32, width: f32| {
+            let line = [to_screen * (points[0] - self.focus_center).to_pos2(), to_screen * (points[1] - self.focus_center).to_pos2()];
+
+            // culling
+            if rect.intersects(Rect::from_two_pos(line[0], line[1])) {
+                shapes.push(Shape::line_segment(line, (width, color)));
+            }
+        };
+        let center = pos2(0.0, 0.0);
+        let end = pos2(0.4, 0.4);
+        paint_line([center, end], Color32::from_additive_luminance(255), 1.0);
+        painter.extend(shapes);
+        //todo!()
+    }
+}
