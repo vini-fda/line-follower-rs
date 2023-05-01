93ab86d8823ebe8edb3ad86afd33e913169d53d7
Vinicius <vinifreitas.d.a@gmail.com>
2023-05-01 01:48:29 -0300

    chore: removed comment

--- path_editor/src/app.rs
+++ path_editor/src/app.rs
@@ -80,36 +80,6 @@ enum ToolType {
     LinePath,
 }
 
-// pub fn draw_closed_curve<F>(points: &[Point2<F>], color: Color, stroke_width: f32)
-// where
-//     F: Float,
-// {
-//     for i in 1..points.len() {
-//         draw_line(
-//             points[i - 1].x.to_f32().unwrap(),
-//             points[i - 1].y.to_f32().unwrap(),
-//             points[i].x.to_f32().unwrap(),
-//             points[i].y.to_f32().unwrap(),
-//             stroke_width,
-//             color,
-//         );
-//     }
-//     draw_line(
-//         points[points.len() - 1].x.to_f32().unwrap(),
-//         points[points.len() - 1].y.to_f32().unwrap(),
-//         points[0].x.to_f32().unwrap(),
-//         points[0].y.to_f32().unwrap(),
-//         stroke_width,
-//         color,
-//     );
-// }
-
-// impl Paintable for SubPath<f64> {
-//     fn paint(&self, ui: &Ui, canvas: &Canvas, painter: &Painter) {
-
-//     }
-// }
-
 impl PathEditorApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         Self {
