a7a765379cb9db976fd3245c4a5997f730c2e5f6
Vinicius <vinifreitas.d.a@gmail.com>
2023-04-30 22:14:53 -0300

    chore: added underline to unused variables

--- path_editor/src/tools/free_tool.rs
+++ path_editor/src/tools/free_tool.rs
@@ -10,5 +10,5 @@ impl Tool for FreeTool {
     fn on_click(&mut self, _p: egui::Pos2) -> Option<linefollower_core::geometry::closed_path::SubPath<f64>> {
         None
     }
-    fn draw(&self, ui: &Ui, canvas: &Canvas, painter: &Painter) {}
+    fn draw(&self, _ui: &Ui, _canvas: &Canvas, _painter: &Painter) {}
 }
